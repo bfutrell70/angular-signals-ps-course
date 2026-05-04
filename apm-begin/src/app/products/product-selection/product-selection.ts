cb664206b66c5dc68168372dbf2076b394743522
Brian Futrell <bfutrel@gmail.com>
2026-05-04 13:19:20 -0400

    module 2 - Changing a Signal's Value

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../product';
 import { ProductData } from '../product-data';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [FormsModule],
+  imports: [FormsModule, DecimalPipe],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
@@ -15,4 +16,14 @@ export class ProductSelection {
   selectedProduct = signal<Product | undefined>(undefined);
   quantity = signal(1);
   products = signal<Product[]>(ProductData.products);
+
+  onDecrease(): void {
+    // using a tertiary operator to return 0 if q is less than or equal to 0,
+    // otherwise return q - 1
+    this.quantity.update(q => q <= 0 ? 0 : q - 1);
+  }
+
+  onIncrease(): void {
+    this.quantity.update(q => q + 1);
+  }
 }

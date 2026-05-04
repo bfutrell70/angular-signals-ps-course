c6f26cad0c631c3ffa876989577373ab78e7f94d
Brian Futrell <bfutrel@gmail.com>
2026-05-04 12:58:06 -0400

    module 2 - Working with Arrays as Signals

--- apm-begin/src/app/products/product-selection/product-selection.ts
+++ apm-begin/src/app/products/product-selection/product-selection.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Product } from '../product';
+import { ProductData } from '../product-data';
 
 @Component({
   selector: 'app-product-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './product-selection.html',
   styleUrl: './product-selection.css'
 })
 export class ProductSelection {
   pageTitle = 'Product Selection';
 
+  quantity = signal(1);
+  products = signal<Product[]>(ProductData.products);
 }
